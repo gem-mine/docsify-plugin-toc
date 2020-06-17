e19c40217809888e9c41efb4e28bc361dc516dac
yaoj <624975@nd.com>
2020-06-17 11:04:35 +0800

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
         test: /\.(s*)css$/,
         use: [
           'style-loader',
